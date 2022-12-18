b7f8d078e66bbcea27aac81a52144126fb7b0651
Pan9Hu <11740305+pan9hu@user.noreply.gitee.com>
2022-12-18 10:35:56 +0800

    update 2022.12.17 10:31

--- app/admin.py
+++ app/admin.py
@@ -1,34 +1,50 @@
 from django.contrib import admin
 from app.models import *
 
+admin.sites.AdminSite.site_header = "Systemd Online"
+admin.sites.AdminSite.site_title = "Admin Dashboard"
+admin.sites.AdminSite.index_title = "Systemd Online"
+
 @admin.register(Agent)
 class AgentAdmin(admin.ModelAdmin):
-    readonly_fields = ('_id', 'ctime', 'utime')
+    search_fields = ('oper_id', )
+    readonly_fields = ('_id', 'ctime', 'utime', )
 
 
 @admin.register(Config)
 class ConfigtAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('oper_id', )
+    readonly_fields = ('ctime', 'utime', )
+
 
 @admin.register(ServiceConfig)
 class ServiceConfigAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('is_use', )
+    search_fields = ('cname',)
+    readonly_fields = ('ctime', 'utime',)
+
 
 @admin.register(ConfigTemplate)
 class ConfigTemplateAdmin(admin.ModelAdmin):
-    pass
+    search_fields  = ('name', 'owner_id', )
+    readonly_fields = ('_id', 'ctime', 'utime', )
+
 
 @admin.register(LogItem)
 class LogItemAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('_id', 'ctime', 'oper_id', 'utime', )
+
 
 @admin.register(Operator)
 class OperatorAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('ctime', 'utime', )
+    search_fields = ('domain', 'name', 'no', 'email', 'email_backup', 'phone', 'username',)
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('synopsis', 'name', )
+    readonly_fields = ('_id', 'ctime', 'utime', )
+
 
 @admin.register(OperatorGroupRef)
 class OperatorGroupRefAdmin(admin.ModelAdmin):
